04b3e17f07fc6af13a01439cb5326bded3124c2f
Ismael González <ismael@potatolondon.com>
2019-12-02 10:00:51 +0000

    Remove revoked keys

--- src/environments/firebase.config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyARNP3jSsloE3hMBTV63dIKWpWIqlXKsxE',
-  authDomain: 'voxe-e6a90.firebaseapp.com',
-  databaseURL: 'https://voxe-e6a90.firebaseio.com',
-  projectId: 'voxe-e6a90',
-  storageBucket: 'voxe-e6a90.appspot.com',
-  messagingSenderId: '798501221800',
-  appId: '1:798501221800:web:d025ec2c019c2675d2199d'
-};
